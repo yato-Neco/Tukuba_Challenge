6e9ba7433713e221e51ca85e018bc0aa9b2b6913
x64neco <yatoneco0015@gmail.com>
2022-07-18 17:14:49 +0900

    lidar test

--- main_program/rust/gpioRobot/src/senser/lidar.rs
+++ main_program/rust/gpioRobot/src/senser/lidar.rs
@@ -4,7 +4,7 @@ use std::time::Duration;
 
 #[test]
 fn lider() {
-    let mut port = serialport::new("/dev/ttyUSB0", 115200)
+    let mut port = serialport::new("COM4", 115200)
         .stop_bits(serialport::StopBits::One)
         .data_bits(serialport::DataBits::Eight)
         .timeout(Duration::from_millis(10))
@@ -14,7 +14,7 @@ fn lider() {
             ::std::process::exit(1);
         });
 
-    let mut serial_buf: Vec<u8> = vec![0; 15000];
+    let mut serial_buf: Vec<u8> = vec![0; 1500];
     //1 2 3 4 5 6
 
     //let mut tmp = [];
@@ -22,14 +22,21 @@ fn lider() {
     loop {
         match port.read(serial_buf.as_mut_slice()) {
             Ok(t) => {
-                println!("{:?}", &serial_buf[..t]);
+                //println!("{:?}", &serial_buf[..t]);
+                u8_tou16(&serial_buf[..t]);
             }
             Err(_e) => {}
         }
     }
 }
 
-/*
 
-
-*/
+fn u8_tou16(title:&[u8]){
+    let title: Vec<u16> = title
+    .chunks_exact(2)
+    .into_iter()
+    .map(|a| u16::from_ne_bytes([a[0], a[1]]))
+    .collect();
+    let title = title.as_slice();
+    println!("{:?}",title);
+}
