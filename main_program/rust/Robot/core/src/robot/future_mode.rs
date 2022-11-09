8459d957d0c56b9c1a56fec267af441966daccb3
@g021c1169 <g021c1169@g.neec.ac.jp>
2022-11-09 22:33:45 +0900

    add new mode

---
+++ main_program/rust/Robot/core/src/robot/future_mode.rs
@@ -0,0 +1,32 @@
+struct Instruction {
+    priority:u8,
+    pub moter_order:u32,
+    block_time:u128,
+}
+struct Instructions {
+    vec:Vec<Instruction>
+}
+
+
+impl Instructions {
+    fn new() -> Self {
+        Self {
+            vec: Vec::new()
+        }
+    }
+
+    fn add() {
+        
+    }
+}
+
+fn auto() {
+
+    Instruction {
+        priority: 0,
+        moter_order: 0xfffff,
+        block_time: 120,
+    };
+
+
+}
