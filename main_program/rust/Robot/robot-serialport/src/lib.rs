a0b7fbcc968ee8ad85426c20d31e6e8d9faba961
@g021c1169 <g021c1169@g.neec.ac.jp>
2022-11-18 08:59:41 +0900

    raspico 対応

--- main_program/rust/Robot/robot-serialport/src/lib.rs
+++ main_program/rust/Robot/robot-serialport/src/lib.rs
@@ -1,5 +1,41 @@
+use std::time::Duration;
+
+use serialport::SerialPort;
+
 pub fn add(left: usize, right: usize) -> usize {
     left + right
+}
+
+///Simple Robot Management Protocol
+/// SRMP
+struct RasPico {
+    port: Box<dyn SerialPort>,
+}
+
+impl RasPico {
+    pub fn new(port: &str, rate: u32) -> Self {
+        let mut port = match serialport::new(port, rate)
+            .stop_bits(serialport::StopBits::One)
+            .data_bits(serialport::DataBits::Eight)
+            .timeout(Duration::from_millis(10))
+            .open()
+        {
+            Ok(p) => p,
+            Err(_) => panic!(),
+        };
+
+        Self { port }
+    }
+
+    pub fn write(&mut self, order: u32) {
+        
+        self.port.write(&order.to_be_bytes()).expect("Write failed!");
+    }
+
+    pub fn check(&mut self) {
+        self.port.write(&0x10FFFFFF_u32.to_be_bytes()).expect("Write failed!");
+
+    }
 }
 
 #[cfg(test)]
--- main_program/rust/Robot/slam/src/lib.rs
+++ main_program/rust/Robot/slam/src/lib.rs
@@ -5,21 +5,21 @@ use mytools::time_sleep;
 
 
 /// 時間更新
-struct SLAM {
+pub struct SLAM {
     // (robot_postion[x,y], lider_data [frequency,distance])
     vec: Vec<((f64, f64), Vec<(f64, f64)>)>,
     max: usize,
 }
 
 impl SLAM {
-    fn new(max: usize) -> Self {
+    pub fn new(max: usize) -> Self {
         Self {
             vec: Vec::with_capacity(max),
             max,
         }
     }
 
-    fn push(&mut self, value: ((f64, f64), Vec<(f64, f64)>)) {
+    pub fn push(&mut self, value: ((f64, f64), Vec<(f64, f64)>)) {
         self.vec.push(value);
         self.del();
     }
@@ -29,7 +29,7 @@ impl SLAM {
         //(self.vec.0.get(self.max - 1), self.vec.1.get(self.max - 1))
     }
 
-    fn obb(&self){
+    pub fn obb(&self){
 
         let slam_data = self.vec.get(0).unwrap();
 
@@ -49,7 +49,7 @@ impl SLAM {
     }
     
 
-    fn del(&mut self) {
+    pub fn del(&mut self) {
         if self.vec.len() > self.max {
             self.vec.remove(0);
         }
