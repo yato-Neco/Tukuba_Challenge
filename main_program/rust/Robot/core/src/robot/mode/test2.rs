09de6b9ffc84b5944d31a9e445cb9dc0f6f8fd4a
@g021c1169 <g021c1169@g.neec.ac.jp>
2023-01-21 01:12:44 +0900

    周波数変更などなど

--- main_program/rust/Robot/core/src/robot/mode/test2.rs
+++ main_program/rust/Robot/core/src/robot/mode/test2.rs
@@ -1,9 +1,11 @@
-use std::time::Duration;
+use std::{time::{Duration, Instant}, collections::HashMap, sync::mpsc::{Sender, Receiver, self}};
 
-use mytools::time_sleep;
+use mytools::{time_sleep, Xtools};
 use robot_gpio::Moter;
+use rthred::{Rthd, sendG};
+use crate::thread_variable;
 
-use crate::robot::{setting::Settings, config};
+use crate::robot::{setting::Settings, config::{self, SenderOrders}};
 
 pub fn test() {
     let setting_file = Settings::load_setting("./settings.yaml");
@@ -11,6 +13,47 @@ pub fn test() {
     let (right_moter_pin, left_moter_pin) = setting_file.load_moter_pins();
 
     let mut moter_controler = Moter::new(right_moter_pin, left_moter_pin);
+
+
+    let mut thread: HashMap<&str, fn(Sender<String>, SenderOrders)> =
+        std::collections::HashMap::new();
+        let (sendr_err_handles, _receiver_err_handle): (Sender<String>, Receiver<String>) = mpsc::channel();
+    
+    let opcode = thread_variable!("operator");
+
+
+    thread.insert("operator", operator);
+
+
+    Rthd::<String>::thread_generate(thread, &sendr_err_handles, &opcode);
+
+
+    
+
+
+    let mut azimuth:u32 = 0;
+    moter_controler.moter_control(0x1FA4FFFF);
+
+    loop {
+
+        
+        match opcode.get("operator").unwrap().1.try_recv() {
+            Ok(e) => {
+                moter_controler.moter_control(e);
+            }
+            Err(_) => {}
+        };
+
+
+        time_sleep(0, 10);
+
+    }
+}
+
+
+
+
+fn operator(panic_msg: Sender<String>, msg: SenderOrders) {
 
     let mut microbit_port = match serialport::new("COM5", 9600)
         .stop_bits(serialport::StopBits::One)
@@ -24,12 +67,8 @@ pub fn test() {
 
     let mut microbit_serial_buf: Vec<u8> = vec![0; 1000];
 
-    let mut azimuth:u32 = 0;
-    moter_controler.moter_control(0x1FA4FFFF);
 
-    loop {
-
-
+    /*
         match microbit_port.read(microbit_serial_buf.as_mut_slice()) {
             Ok(t) => {
                 let data = microbit_serial_buf[..t].to_vec();
@@ -41,13 +80,24 @@ pub fn test() {
     
             Err(_) => {}
         }
+        */
 
 
+    loop {
+
+        sendG(config::FRONT, &msg);
+
+
+        /*
+        
+        
         if azimuth > 90 {
             moter_controler.moter_control(config::STOP);
         }
 
-        time_sleep(0, 10);
+        
+        */
+
 
     }
 }
@@ -55,4 +105,48 @@ pub fn test() {
 
 fn serial() {
     time_sleep(0, 10);
+}
+
+
+#[test]
+fn s() {
+    let v2 = [0.0,1.0,0.0,0.0,-1.0];
+    let mut v = 0.0; 
+
+    let mut timer = Scheduler::start();
+
+    
+    for vv in v2 {
+        let t = timer.end();
+        v +=  vv * t;
+        println!("{} {}",v.roundf(10),t);
+        timer = Scheduler::start();
+        time_sleep(2, 000);
+
+    }
+
+
+}
+
+#[derive(Debug,Clone)]
+pub struct Scheduler {
+    start_time: Instant,
+}
+
+
+impl Scheduler {
+    // カウントを開始する。
+    pub fn start() -> Scheduler {
+        let now_time = Instant::now();
+
+        Scheduler {
+            start_time: now_time,
+        }
+    }
+
+    pub fn end(&self) -> f64 {
+        // 二つ目のカウントが始まって終わった時の時間を、一つ目のカウントでは無かったことにする。
+        self.start_time.elapsed().as_secs_f64()
+
+    }
 }
