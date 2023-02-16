95096ad4beff52f2da84308100167dc18a31927b
yato-Neco <ystoNeco0015@gmail.com>
2023-02-16 17:58:25 +0900

    GPSセンサ更新中

--- main_program/rust/moter_rpm/src/main.rs
+++ main_program/rust/moter_rpm/src/main.rs
@@ -1,6 +1,6 @@
 use core::time::Duration;
 fn main() {
-    let mut port = match serialport::new("/dev/ttyAMA0", 300)
+    let mut port = match serialport::new("/dev/ttyAMA0", 9600)
         .stop_bits(serialport::StopBits::One)
         .data_bits(serialport::DataBits::Eight)
         .timeout(Duration::from_millis(10))
@@ -11,15 +11,23 @@ fn main() {
             panic!()
         }
     };
-    let mut serial_buf: Vec<u8> = vec![0; 1000];
+    let mut serial_buf: Vec<u8> = vec![0; 2000];
+    let mut tmp_vec:Vec<u8> = Vec::new();
 
-
-    loop{
+    loop {
         match port.read(serial_buf.as_mut_slice()) {
             Ok(t) => {
-                let data = serial_buf[..t].to_vec();
+                let mut data = serial_buf[..t].to_vec();
+                tmp_vec.append(&mut data);
+                if tmp_vec.len() > 800 {
+                    let gps_data = String::from_utf8_lossy(&tmp_vec).to_string();
+                    println!("{}:",gps_data);
+                  
+                    tmp_vec.clear();
+                }
+                
 
-                println!("{:?}",data);
+
             }
             Err(_) => {}
         }
