0c9e999b2b49bad924c66064319883778740a36e
x64neco <yatoneco0015@gmail.com>
2023-02-16 13:52:05 +0900

    beta 0.8

--- main_program/rust/Robot/core/src/robot/tui.rs
+++ main_program/rust/Robot/core/src/robot/tui.rs
@@ -49,8 +49,8 @@ pub fn na_ui<B: Backend>(f: &mut Frame<B>, event: &AutoEvents, nav: &Nav, wt901:
         .split(f.size());
 
     let left_block = Paragraph::new(format!(
-        "maneuver: {}\nis_first_time: {}\nis_flash: {}\nstart_azimuth: {}\nnext_azimuth: {}\nis_trune {}\n",
-        event.maneuver, event.is_first_time, event.is_flash, nav.start_azimuth, nav.next_azimuth, event.is_trune
+        "maneuver: {}\nis_first_time: {}\nis_flash: {}\nstart_azimuth: {}\nnext_azimuth: {}\nis_trune {}\nazimuth: {}\ngpss: {} {} {:?}",
+        event.maneuver, event.is_first_time, event.is_flash, nav.start_azimuth, nav.next_azimuth, event.is_trune,event.azimuth,nav.destination_index,nav.waypoints.len(),nav.row_waypoints.get(nav.destination_index)
     ))
     .block(Block::default().borders(Borders::ALL))
     .alignment(tui::layout::Alignment::Left);
