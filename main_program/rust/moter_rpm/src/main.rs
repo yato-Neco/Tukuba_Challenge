8d0cb798507348ea921460959fe5d2ab3109485d
yato-Neco <ystoNeco0015@gmail.com>
2023-02-17 19:04:58 +0900

    gps更新手続き

--- main_program/rust/moter_rpm/src/main.rs
+++ main_program/rust/moter_rpm/src/main.rs
@@ -1,6 +1,6 @@
 use core::time::Duration;
 fn main() {
-    let mut port = match serialport::new("/dev/ttyAMA0", 9600)
+    let mut port = match serialport::new("/dev/ttyUSB0", 115200)
         .stop_bits(serialport::StopBits::One)
         .data_bits(serialport::DataBits::Eight)
         .timeout(Duration::from_millis(10))
@@ -12,22 +12,31 @@ fn main() {
         }
     };
     let mut serial_buf: Vec<u8> = vec![0; 2000];
-    let mut tmp_vec:Vec<u8> = Vec::new();
+    let mut tmp_vec: Vec<u8> = Vec::new();
 
     loop {
         match port.read(serial_buf.as_mut_slice()) {
             Ok(t) => {
                 let mut data = serial_buf[..t].to_vec();
                 tmp_vec.append(&mut data);
-                if tmp_vec.len() > 800 {
+
+                if tmp_vec.len() > 200 {
                     let gps_data = String::from_utf8_lossy(&tmp_vec).to_string();
-                    println!("{}:",gps_data);
-                  
+
+                    let vec: Vec<&str> = gps_data.split("$").collect();
+
+                    for (i, data) in vec.iter().enumerate() {
+                        match data.find("GNRMC") {
+                            Some(p) => {
+                                println!("{:?}", vec[i].replace("ÜC", "").trim());
+                            }
+
+                            None => {}
+                        }
+                    }
+
                     tmp_vec.clear();
                 }
-                
-
-
             }
             Err(_) => {}
         }
